d3fd3e3b58fc3d246639640f02789190f08a5af7
om3ga <om3ga6400gd@gmail.com>
2024-12-13 15:08:29 +0000

    small update

---
+++ js/app.js
@@ -0,0 +1,14 @@
+tsParticles.load("tsparticles", {
+  particles: {
+    size: {
+      value: 1,
+    },
+    line_linked: {
+      enable: true,
+    },
+    move: {
+      enable: true,
+      speed: 1.5,
+    },
+  },
+});
