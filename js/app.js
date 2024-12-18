25049406e90ea9b9be2114ac6e6bdc632426abba
om3ga <om3ga6400gd@gmail.com>
2024-12-18 20:55:17 +0000

    upgrade tsparticles to 3.3.0

--- js/app.js
+++ js/app.js
@@ -1,19 +1,22 @@
-tsParticles.load("tsparticles", {
-  particles: {
-    number: {
-      value: 100,
-    },
-    size: {
-      value: 1,
-    },
-    links: {
-      enable: true,
-      distance: 115,
-      opacity: 0.25,
-    },
-    move: {
-      enable: true,
-      speed: 1.5,
+tsParticles.load({
+  id: "tsparticles",
+  options: {
+    particles: {
+      number: {
+        value: 100,
+      },
+      size: {
+        value: 1,
+      },
+      links: {
+        enable: true,
+        distance: 115,
+        opacity: 0.25,
+      },
+      move: {
+        enable: true,
+        speed: 1.5,
+      },
     },
   },
 });
