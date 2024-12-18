1c3084f61879386db1f2c30d6ae31a335d2bcc55
om3ga <om3ga6400gd@gmail.com>
2024-12-18 19:00:54 +0000

    mostly css changes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 tsParticles.load("tsparticles", {
   particles: {
+    "number": {
+      "value": 100,
+    },
     size: {
       value: 1,
     },
     links: {
       enable: true,
+      distance: 115,
+      opacity: 0.25,
     },
     move: {
       enable: true,
