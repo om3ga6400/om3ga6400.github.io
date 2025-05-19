9ceb0967f2f007188c3557f0e1997e65184beb03
om3ga6400 <JackGA6400@gmail.com>
2025-05-19 13:18:53 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
-
 (async () => {
   await loadFull(tsParticles);
   await tsParticles.load({
     id: "tsparticles",
     options: {
       particles: {
-        number: { value: 80 },
-        size: { value: 1 },
-        color: { value: "#cba6f7" },
+        number: {
+          value: 80
+        },
+        size: {
+          value: 1
+        },
+        color: {
+          value: "#cba6f7"
+        },
         links: {
           enable: true,
           distance: 100,
