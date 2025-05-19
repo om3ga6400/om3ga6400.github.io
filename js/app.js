9d280d2cd602f771be281f92a23c34d448fcddb0
om3ga6400 <JackGA6400@gmail.com>
2025-05-19 10:32:33 -0400

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+(async () => {
+  await loadFull(tsParticles);
+  await tsParticles.load({
+    id: "tsparticles",
+    options: {
+      particles: {
+        number: { value: 80 },
+        size: { value: 1 },
+        color: { value: "#cba6f7" },
+        links: {
+          enable: true,
+          distance: 100,
+          opacity: 0.25,
+          color: "#cba6f7"
+        },
+        move: {
+          enable: true,
+          speed: 1.5,
+          outModes: "bounce"
+        }
+      }
+    }
+  });
+})();
