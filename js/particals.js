d27549076b2e6880302eb7843a23e04d8f772ff9
Ω <JackGA6400@gmail.com>
2025-08-01 04:03:55 -0400

    Update tsparticles to 3.9.0

--- js/particals.js
+++ js/particals.js
@@ -1,3 +1,3 @@
 var script = document.createElement('script');
-script.src = "https://cdn.jsdelivr.net/npm/tsparticles@3.8.1/tsparticles.bundle.min.js";
+script.src = "https://cdn.jsdelivr.net/npm/tsparticles@3.9.0/tsparticles.bundle.min.js";
 document.head.appendChild(script);
