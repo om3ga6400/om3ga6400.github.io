f58e37299b37edd2fbb1284fec02d9b667e0bf1c
Ω <JackGA6400@gmail.com>
2025-08-01 02:06:35 -0400

    lower link distance

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
         number: {value: 125},
         size: {value: 1},
         color: {value: "#f5c2e7"},
-        links: {enable: true, distance: 125, opacity: 0.25, color: "#f5c2e7"},
+        links: {enable: true, distance: 100, opacity: 0.25, color: "#f5c2e7"},
         move: {enable: true, speed: 1.75, outModes: "bounce"}
       }
     }
