884cc34a5340cb5254323107093df69c4ec7947b
Ω <JackGA6400@gmail.com>
2025-08-01 01:55:53 -0400

    try to fix indent

--- js/app.js
+++ js/app.js
@@ -4,12 +4,12 @@
     id: "tsparticles",
     options: {
       particles: {
-				number: {value: 100},
-				size: {value: 1},
-				color: {value: "#f5c2e7"},
+        number: {value: 100},
+        size: {value: 1},
+        color: {value: "#f5c2e7"},
 				links: {enable: true, distance: 125, opacity: 0.25, color: "#f5c2e7"},
 				move: {enable: true, speed: 1.75, outModes: "bounce"}
       }
-		}
+    }
   });
 })();
