550949911175da76b218405bc050ac7161c9ad3f
Ω <JackGA6400@gmail.com>
2025-08-01 01:56:35 -0400

    omfg

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 (async () => {
-  await loadFull(tsParticles);
-  await tsParticles.load({
-    id: "tsparticles",
-    options: {
-      particles: {
-        number: {value: 100},
-        size: {value: 1},
-        color: {value: "#f5c2e7"},
-				links: {enable: true, distance: 125, opacity: 0.25, color: "#f5c2e7"},
-				move: {enable: true, speed: 1.75, outModes: "bounce"}
-      }
-    }
-  });
+await loadFull(tsParticles);
+await tsParticles.load({
+id: "tsparticles",
+options: {
+particles: {
+number: {value: 100},
+size: {value: 1},
+color: {value: "#f5c2e7"},
+links: {enable: true, distance: 125, opacity: 0.25, color: "#f5c2e7"},
+move: {enable: true, speed: 1.75, outModes: "bounce"}
+}
+}
+});
 })();
