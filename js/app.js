ff9cdd87f7f7c2c7766ffb36118b08c12a971f89
Ω <JackGA6400@gmail.com>
2025-09-16 16:51:21 +0000

    Better formatting

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@
     id: "tsparticles",
     options: {
       particles: {
-        number: {value: 125},
-        size: {value: 1},
-        color: {value: "#cba6f7"},
-        links: {enable: true, distance: 90, opacity: 0.25, color: "#cba6f7"},
-        move: {enable: true, speed: 0.8, outModes: "bounce" }
+        number: { value: 125 },
+        size: { value: 1 },
+        color: { value: "#cba6f7" },
+        links: { enable: true, distance: 90, opacity: 0.25, color: "#cba6f7" },
+        move: { enable: true, speed: 0.8, outModes: "bounce" }
       }
     }
   });
