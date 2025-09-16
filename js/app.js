109ec77569a4f20622d2ca2f225ca266400cd639
Ω <JackGA6400@gmail.com>
2025-09-16 16:40:09 +0000

    Tweak some small details of the bg

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@
         number: {value: 125},
         size: {value: 1},
         color: {value: "#cba6f7"},
-        links: {enable: true, distance: 100, opacity: 0.25, color: "#cba6f7"},
-        move: {enable: true, speed: 0.75, outModes: "bounce" }
+        links: {enable: true, distance: 90, opacity: 0.25, color: "#cba6f7"},
+        move: {enable: true, speed: 0.8, outModes: "bounce" }
       }
     }
   });
