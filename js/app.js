cba5d00922381fb1f7b9df5814eed3fe3c1ab9fa
Ω <om3ga6400.dev@gmail.com>
2026-02-10 19:28:40 +0000

    Better background

--- js/app.js
+++ js/app.js
@@ -4,11 +4,20 @@
     id: "tsparticles",
     options: {
       particles: {
-        number: { value: 100 },
+        number: { value: 110 },
         size: { value: 1 },
         color: { value: "#cba6f7" },
         links: { enable: true, distance: 100, opacity: 0.5, color: "#cba6f7" },
         move: { enable: true, speed: 0.5, outModes: "bounce" },
+      },
+      interactivity: {
+        detectsOn: "window",
+        events: {
+          onHover: { enable: true, mode: "grab" },
+        },
+        modes: {
+          grab: { distance: 100 },
+        },
       },
     },
   });
