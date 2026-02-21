6989239617f739e99a5d0abb76c63f51c4796253
Ω <om3ga6400.dev@gmail.com>
2026-02-20 20:47:14 -0500

    Add debug tool

---
+++ js/debug.js
@@ -0,0 +1,2 @@
+window.debugContainerTrue = () => document.querySelectorAll("*").forEach((el) => (el.style.outline = "1px solid red"));
+window.debugContainerFalse = () => document.querySelectorAll("*").forEach((el) => (el.style.outline = ""));
