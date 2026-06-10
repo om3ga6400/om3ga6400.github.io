cbe2e2551d5ae3d635e32250a917c9c8493e8c9e
Ω <om3ga6400@proton.me>
2026-06-10 16:06:40 -0400

    upgrade tsparticles to 4.1.3

--- js/app.js
+++ js/app.js
@@ -9,19 +9,15 @@
           value: 115,
           density: { enable: true, area: 800 },
         },
-        size: { value: 1 },
-        color: { value: "#cba6f7" },
-        links: { enable: true, distance: 100, opacity: 0.5, color: "#cba6f7" },
+        size: { value: 1.5 },
+        paint: {
+          fill: {
+            enable: true,
+            color: { value: "#cba6f7" },
+          },
+        },
+        links: { enable: true, distance: 125, opacity: 0.5, color: "#cba6f7" },
         move: { enable: true, speed: 0.5 },
-      },
-      interactivity: {
-        detectsOn: "window",
-        events: {
-          onHover: { enable: true, mode: "grab" },
-        },
-        modes: {
-          grab: { distance: 100 },
-        },
       },
     },
   });
