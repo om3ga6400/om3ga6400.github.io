46925c72ffa6d181d81db316c7688aeeaf9e020b
Ω <om3ga6400@proton.me>
2026-08-20 16:42:48 -0400

    ugh wtf

--- js/app.js
+++ js/app.js
@@ -3,14 +3,18 @@
   await tsParticles.load({
     id: "tsparticles",
     options: {
-      fullScreen: { enable: false },
       particles: {
         number: {
           value: 100,
           density: { enable: true, area: 500 },
         },
         size: { value: 1 },
-        color: { value: "#cba6f7" },
+        paint: {
+          fill: {
+            enable: true,
+            color: { value: "#cba6f7" },
+          },
+        },
         links: { enable: true, distance: 125, opacity: 0.5, color: "#cba6f7" },
         move: { enable: true, speed: 0.5 },
       },
